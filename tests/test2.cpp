0957c1023bc48c49c4cdfce8055dba961beac20f
Maksym Starkov <253604@student.pwr.edu.pl>
2021-03-28 19:19:57 +0200

    dopisanie doctest 1

--- tests/test2.cpp
+++ tests/test2.cpp
@@ -38,3 +38,37 @@ TEST_CASE("Test LZespolona dzielenie przez skalar - zero") {
    WARN_THROWS(x/t);
 }
 
+TEST_CASE("Test LZespolona - wyswietlanie standart") {
+    LZespolona x;
+    
+    x.re = 2;
+    x.im = 2;
+
+    std::ostringstream out;
+
+    out << x;
+    CHECK( "(2.00+2.00i)" == out.str() );
+}
+
+TEST_CASE("Test LZespolona - wyswietlanie zaokraglanie") {
+    LZespolona x;
+    
+    x.re = 2.0/3.0;
+    x.im = 2.0/3.0;
+
+    std::ostringstream out;
+
+    out << x;
+   CHECK( "(0.67+0.67i)" == out.str() );
+}
+
+TEST_CASE("Test LZespolona - wczytywanie standart") {
+    LZespolona x; 
+
+    std::istringstream in("(10+10.10i)");
+    in >> x;
+    std::ostringstream out;
+    out << x;
+    
+   CHECK( "(10.00+10.10i)" == out.str() );
+}
